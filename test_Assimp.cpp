79690a02696560b5189deda1fb55b60f485e57d9
Aloys <aloys.tetenoire@etu.umontpellier.fr>
2023-04-08 01:32:56 +0200

    Add assimp

---
+++ test_Assimp.cpp
@@ -0,0 +1,13 @@
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+
+int main(int argc, char **argv)
+{
+	Assimp::Importer importer;
+	// const aiScene * scene = importer.ReadFile("torus.fbx", 0);
+	// placer un fichier à l'arrache dans le dossier de build pour faire le test
+
+	return 0;
+}
