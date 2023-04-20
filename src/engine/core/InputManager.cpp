5525754f2b3da94e0b4241346bb75c1aeacb763e
LIRZIN <lirzinleo@gmail.com>
2023-04-20 12:29:49 +0200

    InputManager

--- src/engine/core/InputManager.cpp
+++ src/engine/core/InputManager.cpp
@@ -5,18 +5,45 @@ namespace teliod::core
 {
 	InputManager & InputManager::getInstance()
 	{
-		static InputManager manager;
-		return manager;
+		return inputManager;
 	}
 
-	void InputManager::init(GLFWwindow *pWindow)
-	{
-		window = pWindow;
-	}
+    void InputManager::init(GLFWwindow *pWindow)
+    {
+        window = pWindow;
+
+        glfwSetKeyCallback( window, UpdateCallback );
+    }
+
+    void* InputManager::UpdateCallback( GLFWwindow* _window, int key, int scancode, int action, int mods )
+    {
+        for( auto& ActionKey : action_mappings )
+        {
+            if( key == (ActionKey.second&DisableMask) && action == GLFW_PRESS )
+                ActionKey.second |= EnableMask;
+            else
+                ActionKey.second &= DisableMask;
+        }
+    }
+    
+    void InputManager::MapAction( std::string& action, int key )
+    {
+        if( action.length() == 0 || key == GLFW_KEY_UNKNOWN )
+            return;
+
+        action_mappings[action] = key;
+
+        UpdateCallback( action, key );
+    }
 
 	bool InputManager::isKeyPressed(int key) const
 	{
-		return glfwGetKey(window, key) == GLFW_PRESS;
+		return glfwGetKey( window, key ) == GLFW_PRESS;
 	}
+
+    bool InputManager::isActionDone(std::string& action) const
+    {
+        return action_mappings.at(action)&EnableMask;
+    }
 
 }
--- src/engine/core/include/core/InputManager.h
+++ src/engine/core/include/core/InputManager.h
@@ -3,18 +3,37 @@
 
 class GLFWwindow;
 
+#include <iostream>
+#include <string>
+#include <bitset>
+#include <unordered_map>
+
 namespace teliod::core
 {
 	class InputManager
 	{
-	public:
-		static InputManager & getInstance();
-		void init(GLFWwindow * pWindow);
+        static InputManager inputManager;
 
-		bool isKeyPressed(int key) const;
-	private:
-		GLFWwindow * window;
-	};
+        //std::bitset<64> input;
+        const unsigned short EnableMask = 0x8000;
+        const unsigned short DisableMask = 0x7fff;
+        std::unordered_map< std::string, unsigned short > action_mappings;
+
+        void* UpdateCallback( GLFWwindow* _window, int key, int scancode, int action, int mods );
+
+
+
+        public:
+            static InputManager & getInstance();
+            void init(GLFWwindow * pWindow);
+            void MapAction( std::string& action, int key );
+
+            bool isKeyPressed( int key ) const;
+            bool isActionDone( std::string& action ) const;
+
+        private:
+            GLFWwindow * window;
+    };
 }
 
 #endif // TELIOD_CORE_INPUTMANAGER_H
