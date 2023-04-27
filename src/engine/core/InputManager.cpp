ea29ba9f2662d002569036994e1654d90a129dd4
Aloys <aloys.tetenoire@etu.umontpellier.fr>
2023-04-27 10:17:34 +0200

    Add glfw callbacks

--- src/engine/core/InputManager.cpp
+++ src/engine/core/InputManager.cpp
@@ -9,9 +9,25 @@ namespace teliod::core
 		return manager;
 	}
 
+	InputManager::InputManager()
+	: window(nullptr)
+	, isHoveringWindow(false)
+	{
+
+	}
+
 	void InputManager::init(GLFWwindow *pWindow)
 	{
 		window = pWindow;
+		glfwSetKeyCallback(window, [](GLFWwindow * _window, int key, int scancode, int action, int mods){
+			InputManager::getInstance().keyCallback(_window, key, scancode, action, mods);
+		});
+		glfwSetMouseButtonCallback(window, [](GLFWwindow * _window, int button, int action, int mods){
+			InputManager::getInstance().mouseButtonCallback(_window, button, action, mods);
+		});
+		glfwSetCursorEnterCallback(window, [](GLFWwindow * _window, int entered){
+			InputManager::getInstance().mouseEnterCallback(_window, entered);
+		});
 	}
 
 	bool InputManager::isKeyPressed(int key) const
@@ -19,4 +35,39 @@ namespace teliod::core
 		return glfwGetKey(window, key) == GLFW_PRESS;
 	}
 
+	void InputManager::captureMouse()
+	{
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+		if (glfwRawMouseMotionSupported())
+			glfwSetInputMode(window, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+	}
+
+	void InputManager::releaseMouse()
+	{
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+	}
+
+	void InputManager::keyCallback(GLFWwindow * _window, int key, int scancode, int action, int mods)
+	{
+		if (key == GLFW_KEY_ESCAPE)
+		{
+			releaseMouse();
+		}
+	}
+
+	void InputManager::mouseButtonCallback(GLFWwindow * _window, int button, int action, int mods)
+	{
+		if (!isHoveringWindow)
+			return;
+
+		if (isHoveringWindow && button == GLFW_MOUSE_BUTTON_LEFT)
+		{
+			captureMouse();
+		}
+	}
+
+	void InputManager::mouseEnterCallback(GLFWwindow *_window, int entered)
+	{
+		isHoveringWindow = entered == GLFW_TRUE;
+	}
 }
--- src/engine/core/include/core/InputManager.h
+++ src/engine/core/include/core/InputManager.h
@@ -12,7 +12,21 @@ namespace teliod::core
 		void init(GLFWwindow * pWindow);
 
 		bool isKeyPressed(int key) const;
+
+		void captureMouse();
+		void releaseMouse();
+
+		~InputManager() = default;
 	private:
+		InputManager();
+		void keyCallback(GLFWwindow * _window, int key, int scancode, int action, int mods);
+		void mouseButtonCallback(GLFWwindow * _window, int button, int action, int mods);
+		void mouseEnterCallback(GLFWwindow * _window, int entered);
+
+		// state
+		bool isHoveringWindow;
+		bool isCursorCaptured;
+
 		GLFWwindow * window;
 	};
 }
